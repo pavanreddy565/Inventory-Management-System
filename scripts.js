e075b6dda68347db920b31f1344ea25d296eb28f
pavanreddy565 <pavanreddy5658@gmail.com>
2024-02-11 20:24:57 +0530

    basic layout of page completed

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+const toggleBtn = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
